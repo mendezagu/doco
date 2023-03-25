6746b97d9bf489af9e3ca8060a3ecd55afb40546
zagmen <mendezagustin.ma2@gmail.com>
2023-03-25 01:16:01 -0300

    commit

---
+++ Aplicaciones/Clientes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Clientes(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField(max_length=254)
+    telefono = models.CharField(max_length=20)
+    documento = models.CharField(max_length=20)
+    direccion = models.CharField(max_length=100)
+    parentesco = models.CharField(max_length=100)
+    menorNombre = models.CharField(max_length=100)
+    menorApellido = models.CharField(max_length=100)
+    fecha_nacimientoMenor = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
---
+++ Aplicaciones/Clientes/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,redirect
+from .models import Clientes
+from django.contrib import messages
+
+def home(request):
+    return render(request, "formulario.html")
+
+def registrarUsuario(request):
+    nombre = request.POST['txtNombre']
+    apellido = request.POST['txtApellido']
+    email = request.POST['txtEmail']
+    telefono = request.POST['txtTelefono']
+    documento = request.POST['txtDocumento']
+    direccion = request.POST['txtDireccion']
+    menorNombre = request.POST['txtApellidoMenor']
+    menorApellido = request.POST['txtTelefono']
+    parentesco = request.POST['txtParentesco']
+    fecha_nacimientoMenor = request.POST['txtFeNacimiento']
+
+    
+
+    cliente = Clientes.objects.create(
+        apellido=apellido, 
+        nombre=nombre,
+        email=email,
+        telefono=telefono,
+        documento=documento,
+        direccion=direccion,
+        menorNombre=menorNombre,
+        menorApellido=menorApellido,
+        parentesco=parentesco,
+        fecha_nacimientoMenor=fecha_nacimientoMenor
+
+        )
+    
+
+    messages.success(request, '¡Cliente registrado!')
+    return redirect('/')
